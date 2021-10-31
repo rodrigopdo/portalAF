385c1786219ca98ff2cae1a2060ba6efcd251e68
rodrigopdo <rodrigo.prudente82@gmail.com>
2021-10-31 12:55:37 -0300

    table done

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,33 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { TableContainer } from './styles';
+import { PaginationItem, TableContainer, TablePagination } from './styles';
 
 const Table = props => {
+
+  const initDataShow = props.limit && props.bodyData ? props.bodyData.slice(0, Number(props.limit)) : props.bodyData
+
+  const [dataShow, setDataShow] = useState(initDataShow);
+
+  let pages = 1;
+
+  let range = [];
+
+  if(props.limit !== undefined) {
+    let page = Math.floor(props.bodyData.length / Number(props.limit));
+    pages = props.bodyData.length % Number(props.limit) === 0 ? page : page + 1;
+    range = [...Array(pages).keys()];
+  }
+
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const selectPage = page => {
+    const start = Number(props.limit) * page
+    const end = start + Number(props.limit)
+
+    setDataShow(props.bodyData.slice(start, end))
+
+    setCurrentPage(page)
+  }
+
+
   return (
-    <TableContainer>
-      <table>
-        {
-          props.headData && props.renderHead ? (
-            <thead>
-              <tr>
+    <div>
+      <TableContainer>
+        <table>
+          {
+            props.headData && props.renderHead ? (
+              <thead>
+                <tr>
+                  {
+                    props.headData.map((item, index) => props.renderHead(item, index))
+                  }
+                </tr>
+              </thead>
+            ) : null
+          }
+          {
+            props.bodyData && props.renderBody ? (
+              <tbody>
                 {
-                  props.headData.map((item, index) => props.renderHead(item, index))
+                  dataShow.map((item, index) => props.renderBody(item, index))
                 }
-              </tr>
-            </thead>
-          ) : null
-        }
-        {
-          props.bodyData && props.renderBody ? (
-            <tbody>
-              {
-                props.bodyData.map((item, index) => props.renderBody(item, index))
-              }
-            </tbody>
-          ) : null
-        }
-      </table>
-    </TableContainer>
+              </tbody>
+            ) : null
+          }
+        </table>
+      </TableContainer>
+      {
+        pages > 1 ? (
+          <TablePagination>
+            {
+              range.map((item, index) => (
+                <PaginationItem key={index} className={currentPage === index ? 'active' : ''} onClick={() => selectPage(index)}>
+                  {item + 1}
+                </PaginationItem>
+              ))
+            }
+          </TablePagination>
+        ) : null
+      }
+    </div>
   )
 }
 
--- src/components/Table/styles.js
+++ src/components/Table/styles.js
@@ -22,3 +22,38 @@ export const TableContainer = styled.div `
     color: #ffffff;
   }
 `;
+
+export const TablePagination = styled.div `
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const PaginationItem = styled.div `
+  margin-left: 10px;
+  padding-top: 4px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  cursor: pointer;
+  &.active {
+    background-color: #3DD969;
+    color: #ffffff;
+    font-weight: 600;
+  }
+  &.active:hover {
+    background-color: #3DD969;
+    color: #ffffff;
+    font-weight: 600;
+  }
+  &:hover {
+    color: #000;
+    background-color: #f4f4f4;
+  }
+`;
+
